d7a5cc3aa4485138c5fca607933201af0c40f07b
dulton <uzairhyd89@gmail.com>
2022-08-05 22:31:42 +0530

    Added the rest

--- blogposts/models.py
+++ blogposts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    post_title = models.CharField(max_length=300)
+    post_body = models.TextField()
+    post_date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.post_title
+
--- blogposts/views.py
+++ blogposts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .serializers import PostSerializer
+from .models import Post
+
+
+@api_view(['GET'])
+def posts(request):
+    posts = Post.objects.all()
+    serializer = PostSerializer(posts, many=True)
+    return Response(serializer.data)
