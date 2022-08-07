7b5f4219aeb83ce914b72e6ac812ca360fb3282a
dulton <uzairhyd89@gmail.com>
2022-08-07 22:58:04 +0530

    updated the test cases

--- blogposts/tests.py
+++ blogposts/tests.py
@@ -2,6 +2,12 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .models import Post, Category
 
+#to test the api
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
 
 class Test_Create_Post(TestCase):
 
@@ -25,5 +31,20 @@ class Test_Create_Post(TestCase):
         self.assertEqual(status, 'published')
         self.assertEqual(str(post), 'Post Title')
         self.assertEqual(str(cat), 'django')
+
+#API test
+class PostTests(APITestCase):
+    def test_view_posts(self):
+        url = reverse('blog_api:listcreate')
+        resonse = self.client.get(url, format='json')
+        self.assertEqual(resonse.status_code, status.HTTP_200_OK)
+
+    def create_post(self):
+        self.test_category = Category.objects.create(name='django')
+        self.testuser1 = User.objects.create_user(username='test_user1', password='123456789')
+        data = {"title": "new", "author": 1, "excerpt": "new", "content": "new"}
+        url = reverse('blog_api:listcreate')
+        resonse = self.client.post(url, data, format='json')
+        self.assertEqual(resonse.status_code, status.HTTP_201_CREATED)
 
 
--- blogposts/urls.py
+++ blogposts/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from .views import *
 
+app_name = 'blog_api'
+
 urlpatterns = [
     # path('', get_posts),
     # path('<str:pk>/', get_post),
